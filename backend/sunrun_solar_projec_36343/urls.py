43ecc9cd81407c283595fc776afc68e7406a4a7e
Crowdbotics <team@crowdbotics.com>
2022-07-21 20:31:32 +0000

    Update admin title

--- backend/sunrun_solar_projec_36343/urls.py
+++ backend/sunrun_solar_projec_36343/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "SunRun Solar project-2"
-admin.site.site_title = "SunRun Solar project-2 Admin Portal"
-admin.site.index_title = "SunRun Solar project-2 Admin"
+admin.site.site_header = "Quiet Unit"
+admin.site.site_title = "Quiet Unit Admin Portal"
+admin.site.index_title = "Quiet Unit Admin"
 
 # swagger
 api_info = openapi.Info(
