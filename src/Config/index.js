e64e457c7bc5828efe0d9ff282d95207cbf3c2eb
Borhan Tipu <me@imtipu.me>
2022-10-13 00:20:13 +0600

    Customer Tracker model unique constraint added for active project

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,5 +1,5 @@
 export const Config = {
   API_URL: 'https://jsonplaceholder.typicode.com/',
-  // API_DEV_URL: 'https://sunrun-solar-projec-36343.botics.co/',
-  API_DEV_URL: 'http://192.168.0.113:8000/',
+  API_DEV_URL: 'https://sunrun-solar-projec-36343.botics.co/',
+  // API_DEV_URL: 'http://192.168.0.113:8000/',
 }
