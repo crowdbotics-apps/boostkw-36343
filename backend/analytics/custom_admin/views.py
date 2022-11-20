15f51262cc63dfbf75407c9a2794acedb2ac4d11
Borhan Tipu <me@imtipu.me>
2022-11-20 21:36:38 +0600

    Graph 1, Graph 3, Graph 4 Updated

--- backend/analytics/custom_admin/views.py
+++ backend/analytics/custom_admin/views.py
@@ -23,7 +23,8 @@ def analytics_index(request):
 @login_required
 @staff_member_required
 def admin_crew_graph_by_location(request):
-    queryset = Crew.objects.annotate(**crew_counter_subqs())
+    # queryset = Crew.objects.annotate(**crew_counter_subqs())
+    queryset = Crew.objects.all()
     # print(queryset)
     result = queryset.aggregate(
         total_job_process=models.Count('customer_trackers__job_processes'),
@@ -35,62 +36,44 @@ def admin_crew_graph_by_location(request):
             'customer_trackers__job_processes',
             filter=models.Q(customer_trackers__location='ground')
         ),
-        # avg_seconds_worked_job=models.ExpressionWrapper(
-        #     models.F('seconds_worked') / models.F('total_job_process'),
-        #     output_field=models.DecimalField(decimal_places=2, default=0),
-        # ),
-        # avg_seconds_worked_job_roof=models.Case(
-        #     models.When(
-        #         customer_trackers__location='roof',
-        #         then=models.F('seconds_worked') / models.F('total_job_process')
-        #     ),
-        #     output_field=models.DecimalField(decimal_places=2, default=0),
-        #     default=0
-        #
-        # ),
-        # avg_seconds_worked_job_ground=models.Case(
-        #     models.When(
-        #         customer_trackers__location='ground',
-        #         then=models.F('seconds_worked') / models.F('total_job_process')
-        #     ),
-        #     output_field=models.DecimalField(decimal_places=2, default=0),
-        #     default=0
-        #
-        # ),
-        total_seconds_worked=models.Sum('seconds_worked'),
-        total_seconds_worked_roof=models.Sum('seconds_worked', filter=models.Q(customer_trackers__location='roof')),
-        total_seconds_worked_ground=models.Sum('seconds_worked', filter=models.Q(customer_trackers__location='ground')),
-        avg_seconds_worked=models.Avg('seconds_worked'),
-        avg_seconds_worked_roof=models.Avg('seconds_worked', filter=models.Q(customer_trackers__location='roof')),
-        avg_seconds_worked_ground=models.Avg('seconds_worked', filter=models.Q(customer_trackers__location='ground')),
+        total_seconds_per_job=models.Sum('customer_trackers__job_processes__seconds_per_job'),
+        total_seconds_per_job_roof=models.Sum('customer_trackers__job_processes__seconds_per_job',
+                                              filter=models.Q(customer_trackers__location='roof')),
+        total_seconds_per_job_ground=models.Sum('customer_trackers__job_processes__seconds_per_job',
+                                                filter=models.Q(customer_trackers__location='ground')),
+        avg_seconds_per_job=models.Avg('customer_trackers__job_processes__seconds_per_job'),
+        avg_seconds_per_job_roof=models.Avg('customer_trackers__job_processes__seconds_per_job',
+                                            filter=models.Q(customer_trackers__location='roof')),
+        avg_seconds_per_job_ground=models.Avg('customer_trackers__job_processes__seconds_per_job',
+                                              filter=models.Q(customer_trackers__location='ground')),
     )
     print(result)
-    total_minutes_worked_roof = result['total_seconds_worked_roof'] / 60
-    total_minutes_worked_ground = result['total_seconds_worked_ground'] / 60
-    total_minutes_worked = result['total_seconds_worked'] / 60
+    total_minutes_per_job_roof = result['total_seconds_per_job_roof'] / 60
+    total_minutes_per_job_ground = result['total_seconds_per_job_ground'] / 60
+    total_minutes_per_job = result['total_seconds_per_job'] / 60
 
-    avg_minutes_worked_roof = 0
-    avg_minutes_worked_ground = 0
-    avg_minutes_worked = 0
+    avg_minutes_per_job_roof = 0
+    avg_minutes_per_job_ground = 0
+    avg_minutes_seconds_per_job = 0
     if result['total_job_process_roof'] and result['total_job_process_roof'] > 0:
-        avg_minutes_worked_roof = result['total_seconds_worked_roof'] / result['total_job_process_roof'] / 60
+        avg_minutes_per_job_roof = result['total_seconds_per_job_roof'] / result['total_job_process_roof'] / 60
 
-    if result['total_seconds_worked_ground'] and result['total_seconds_worked_ground'] > 0:
-        avg_minutes_worked_roof = result['total_seconds_worked_ground'] / result['total_job_process_ground'] / 60
+    if result['total_seconds_per_job_ground'] and result['total_seconds_per_job_ground'] > 0:
+        avg_minutes_per_job_ground = result['total_seconds_per_job_ground'] / result['total_job_process_ground'] / 60
 
     if result['total_job_process'] and result['total_job_process'] > 0:
-        avg_minutes_worked = result['total_seconds_worked'] / result['total_job_process'] / 60
+        avg_minutes_per_job = result['total_seconds_per_job'] / result['total_job_process'] / 60
 
     data = {
         # 'queryset': queryset,
         'title': 'Crew Graph by Location',
         'result': result,
-        'total_minutes_worked_roof': round(total_minutes_worked_roof),
-        'total_minutes_worked_ground': round(total_minutes_worked_ground),
-        'total_minutes_worked': round(total_minutes_worked),
-        'avg_minutes_worked_roof': round(avg_minutes_worked_roof),
-        'avg_minutes_worked_ground': round(avg_minutes_worked_ground),
-        'avg_minutes_worked': round(avg_minutes_worked),
+        'total_minutes_per_job_roof': round(total_minutes_per_job_roof),
+        'total_minutes_per_job_ground': round(total_minutes_per_job_ground),
+        'total_minutes_per_job': round(total_minutes_per_job),
+        'avg_minutes_per_job_roof': round(avg_minutes_per_job_roof),
+        'avg_minutes_per_job_ground': round(avg_minutes_per_job_ground),
+        'avg_minutes_per_job': round(avg_minutes_seconds_per_job),
     }
 
     return render(request, 'analytics/admin/graphs/crew_mins_by_location.html', data)
@@ -102,52 +85,53 @@ def graph_duration_steps(request):
     from trackers.db_utils import job_process_time_spend_seconds_sub_qs
     initial_job_processes = InitialJobProcess.objects.all()
     processes = [{'id': item.id, 'title': item.title, 'type': item.location} for item in initial_job_processes]
-    job_processes = JobProcess.objects.annotate(
-        time_spent_seconds=job_process_time_spend_seconds_sub_qs(),
-        number_of_workers=models.F('customer_tracker__number_of_workers')
-    ).annotate(
-        total_workers_second=models.ExpressionWrapper(
-            models.F('time_spent_seconds') * models.F('number_of_workers'),
-            output_field=models.IntegerField(default=0)
-        )
-    )
-    subqs = {}
+    trackers = CustomerTracker.objects.filter().order_by('-location')
+    subqs_trackers = {}
     for item in processes:
         _id = item.get('id')
         title = item.get('title')
-        subqs[f'count_{_id}'] = models.Count('id', filter=models.Q(title__exact=title),
-                                             output_field=models.IntegerField(default=0))
-        subqs[f'total_workers_second_{_id}'] = models.Sum('total_workers_second', filter=models.Q(title__exact=title),
-                                                          output_field=models.IntegerField(default=0))
+        subqs_trackers[f'count_{_id}'] = models.Count('id', filter=models.Q(job_processes__title__exact=title),
+                                                      output_field=models.IntegerField(default=0))
 
-    results = job_processes.aggregate(**subqs)
+        subqs_trackers[f'total_seconds_per_job_{_id}'] = models.Sum('job_processes__seconds_per_job',
+                                                                    filter=models.Q(job_processes__title__exact=title),
+                                                                    output_field=models.IntegerField(default=0)
+                                                                    )
 
-    roof_list = []
-    ground_list = []
+    results_trackers = trackers.aggregate(**subqs_trackers)
+
+    trackers_roof_list = []
+    trackers_ground_list = []
     for item in processes:
         _id = item.get('id')
-        count_value = results[f'count_{_id}']
-        total_workers_second = results[f'total_workers_second_{_id}']
-        total_workers_mins = round(total_workers_second / 60)
+        count_value = results_trackers[f'count_{_id}']
+        total_seconds_per_job = results_trackers[f'total_seconds_per_job_{_id}']
+        total_seconds_per_job_mins = round(total_seconds_per_job / 60)
         new_item = next((x for x in processes if x['id'] == _id), None)
         new_item['total'] = count_value
-        new_item['total_workers_second'] = total_workers_second
-        new_item['total_workers_mins'] = total_workers_mins
+        new_item['total_seconds_per_job'] = total_seconds_per_job
+        new_item['total_seconds_per_job_mins'] = total_seconds_per_job_mins
+        if int(count_value) > 0:
+            new_item['avg_total_seconds_per_job_mins'] = total_seconds_per_job_mins / int(count_value)
+        else:
+            new_item['avg_total_seconds_per_job_mins'] = 0
 
         if new_item['type'] == 'roof':
-            roof_list.append(new_item)
+            trackers_roof_list.append(new_item)
         elif new_item['type'] == 'ground':
-            ground_list.append(new_item)
+            trackers_ground_list.append(new_item)
 
+    # print(trackers_roof_list)
+    # print(trackers_ground_list)
     data = {
-        'title': 'Duration of Steps (in mins)',
-        'results': results,
-        'roof_list': roof_list,
-        'roof_count': len(roof_list),
-        'ground_list': ground_list,
-        'ground_count': len(ground_list),
+        'title': 'Duration of Steps (Avg in mins)',
+        'results': results_trackers,
+        'roof_list': trackers_roof_list,
+        'roof_count': len(trackers_roof_list),
+        'ground_list': trackers_ground_list,
+        'ground_count': len(trackers_ground_list),
     }
-    # print(data)
+
     return render(request, 'analytics/admin/graphs/duration_of_steps.html', data)
 
 
@@ -156,66 +140,17 @@ def graph_duration_steps(request):
 def admin_roof_type_graph_view(request):
     from roofs.models import RoofType
     from roofs.db_utils import roof_type_counter_subqs
-    # trackers = CustomerTracker.objects.filter(
-    #     roof_type=models.OuterRef('pk'),
-    #     status=CustomerTracker.STATUS_CLOSED
-    # ).annotate(
-    #     seconds_per_job=models.ExpressionWrapper(
-    #         models.Sum('job_processes__seconds_per_job') / models.F('system_size'),
-    #         output_field=models.IntegerField()
-    #     ),
-    # )
-    # seconds_per_kw_subqs = models.Subquery(
-    #     CustomerTracker.objects.filter(
-    #         roof_type=models.OuterRef('pk'),
-    #         status=CustomerTracker.STATUS_CLOSED
-    #     ).annotate(
-    #         seconds_per_kw=models.ExpressionWrapper(
-    #             models.Sum('job_processes__seconds_per_job') / models.F('system_size'),
-    #             output_field=models.IntegerField()
-    #         ),
-    #         hours_per_kw=models.ExpressionWrapper(
-    #             models.Sum('job_processes__seconds_per_job') / 60 / models.F('system_size'),
-    #             output_field=models.IntegerField()
-    #         )
-    #     ).values('seconds_per_kw')[:1],
-    #     output_field=models.IntegerField()
-    #
-    # )
-
-    from django.db.models.functions import Coalesce
-    # subqs = CustomerTracker.objects.filter(
-    #     # roof_type=models.OuterRef('pk'),
-    #     roof_type=3,
-    #     status=CustomerTracker.STATUS_CLOSED
-    # ).annotate(
-    #     seconds_per_kw=models.ExpressionWrapper(
-    #         models.Sum('job_processes__seconds_per_job') / models.F('system_size'),
-    #         output_field=models.IntegerField()
-    #     ),
-    #     # hours_per_kw=models.ExpressionWrapper(
-    #     #     models.Sum('job_processes__seconds_per_job') / 60 / models.F('system_size'),
-    #     #     output_field=models.IntegerField()
-    #     # )
-    #     # ).values('seconds_per_kw')[:1]
-    # ).aggregate(avg=Coalesce(models.Avg('seconds_per_kw'), 0)).get('avg', 0)
-
-    from modules.database_utils import SubqueryAvg, SubquerySum
     roof_types = RoofType.objects.filter(
         customer_trackers__status=CustomerTracker.STATUS_CLOSED,
     ).annotate(
         **roof_type_counter_subqs()
     ).annotate(
-        # avg_seconds_per_kw=models.Subquery(subqs, output_field=models.FloatField()),
         avg_seconds_per_kw=models.Avg('customer_trackers__seconds_per_kw')
-        # avg_seconds_per_kw=models.Exists(
-        #     CustomerTracker.objects.filter(roof_type=models.OuterRef('pk'))
-        # )
     )
 
     chart_data = []
     for roof in roof_types:
-        print(roof.avg_seconds_per_kw)
+        # print(roof.avg_seconds_per_kw)
         hours = seconds_to_readable_time(roof.avg_seconds_per_kw).get('hours')
         chart_data.append({
             'name': roof.name,
