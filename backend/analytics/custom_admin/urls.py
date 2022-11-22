868c085aa3c251cb336d522103fac90b48d2c325
Borhan Tipu <me@imtipu.me>
2022-11-22 23:01:59 +0600

    Performance by Crew added

--- backend/analytics/custom_admin/urls.py
+++ backend/analytics/custom_admin/urls.py
@@ -6,9 +6,14 @@ app_name = 'admin_analytics'
 urlpatterns = [
     path('', analytics_index, name='analytics_index'),
     path('db_url/', admin_db_url, name='admin_db_url'),
+    # graph 1
     path('crew/location/duration-steps/', admin_crew_graph_duration_steps, name='admin_crew_graph_duration_steps'),
+    # graph 2
     path('crew/graph/mix-by-location/', admin_crew_mix_by_location, name='admin_crew_grap_mix_by_location'),
+    # graph 3
     path('crew/performance/', admin_crew_performance_graph, name='admin_crew_performance_graph'),
+    # graph 8
+    path('crew/graph/performance-by-crew/', admin_grap_performance_by_crew, name='admin_grap_performance_by_crew'),
     path('roof-types/graph/', admin_roof_type_graph_view, name='roof_type_graph_view'),
     path('feedbacks/graph/', admin_graph_app_feedback, name='admin_graph_app_feedback'),
 
